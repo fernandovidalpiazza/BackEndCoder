e3446896e357d6a6c194253585754d2f8b847dc8
fernandovidalpiazza <fervidalpiazza@gmail.com>
2024-04-13 15:32:29 -0300

    codigo con Ignore

--- src/3Entrega/app.js
+++ src/3Entrega/app.js
@@ -6,41 +6,35 @@ const manager = new ProductManager("./src/2entrega/productos.txt");
 const PORT = 3000;
 
 app.get("/products", async (req, res) => {
-    
-    const limit = +req.query.limit || 0;
+  const limit = +req.query.limit || 0;
 
-   
-    const productList = manager.getProducts(limit);
+  const productList = manager.getProducts(limit);
 
-    
-    res.send({ status: 1, payload: productList });
+  res.send({ status: 1, payload: productList });
 });
 
-
-
-
 app.get("/products/:id", async (req, res) => {
-    try {
-        // pongo el id en el navegador
-        const productId = req.params.id;
+  try {
+    // pongo el id en el navegador
+    const productId = req.params.id;
 
-        // Obtengo el producto del manger
-        const product = await manager.getProductById(productId);
+    // Obtengo el producto del manger
+    const product = await manager.getProductById(productId);
 
-        // si encuentra el producto ?
-        if (product) {
-            // encuentra 
-            res.status.json({ status: 1, payload: product });
-        } else {
-            // no encuentra 
-            res.status.json({ status: 0, error: "Producto no encontrado" });
-        }
-    } catch (error) {
-        // paso algo que un error 
-        res.status.json({ status: 0, error: "Error al obtener el producto" });
+    // si encuentra el producto ?
+    if (product) {
+      // encuentra
+      res.status.json({ status: 1, payload: product });
+    } else {
+      // no encuentra
+      res.status.json({ status: 0, error: "Producto no encontrado" });
     }
+  } catch (error) {
+    // paso algo que un error
+    res.status.json({ status: 0, error: "Error al obtener el producto" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log("Estoy escuchando en el puerto", PORT);
+  console.log("Estoy escuchando en el puerto", PORT);
 });
